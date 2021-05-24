7cc76ba4bd800b6d9ab93aa1e3243294efcc3a46
uS-aito <h2so40627@gmail.com>
2021-05-25 01:17:27 +0900

    application gatewayの背後にvmssを配置するよう変更

--- azure/virtualnetworks.tf
+++ azure/virtualnetworks.tf
@@ -12,9 +12,16 @@ resource "azurerm_subnet" "frontendsubnet" {
   address_prefixes = [ "10.0.0.0/24" ]
 }
 
+resource "azurerm_subnet" "backendsubnet" {
+  name = "backendsubnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.webvirtualnetwork.name
+  address_prefixes = [ "10.0.1.0/24" ]
+}
+
 resource "azurerm_subnet" "bastionsubnet" {
   name = "AzureBastionSubnet"
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.webvirtualnetwork.name
-  address_prefixes = [ "10.0.1.0/24" ]
+  address_prefixes = [ "10.0.100.0/24" ]
 }
--- azure/webserver.tf
+++ azure/webserver.tf
@@ -1,45 +1,3 @@
-resource "azurerm_public_ip" "webserverpip" {
-  name = "webserverpip"
-  location = "japaneast"
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Dynamic"
-}
-
-resource "azurerm_network_security_group" "webservernsg" {
-  name = "webservernsg"
-  location = "japaneast"
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "webservernic" {
-  name = "webservernic"
-  location = "japaneast"
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name = "webservernic_configuration"
-    subnet_id = azurerm_subnet.frontendsubnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.webserverpip.id
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "webservernicassociation" {
-  network_interface_id = azurerm_network_interface.webservernic.id
-  network_security_group_id = azurerm_network_security_group.webservernsg.id
-}
 resource "tls_private_key" "webserverssh" {
   algorithm = "RSA"
   rsa_bits = 4096
@@ -53,33 +11,6 @@ output "tls_private_key" {
 data "azurerm_image" "webserverimage" {
   resource_group_name = "imagesourcerg"
   name = "webserverimagesource-image-20210522182910"
-}
-
-resource "azurerm_linux_virtual_machine" "webserver" {
-  name = "webserver"
-  location = "japaneast"
-  resource_group_name = azurerm_resource_group.rg.name
-  network_interface_ids = [ 
-    azurerm_network_interface.webservernic.id
-  ]
-  size = "Standard_B2ms"
-
-  os_disk {
-    name = "webserverosdisk"
-    caching = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_id = data.azurerm_image.webserverimage.id
-
-  computer_name = "webserver"
-  admin_username = "azureuser"
-  disable_password_authentication = true
-  
-  admin_ssh_key {
-    username = "azureuser"
-    public_key = tls_private_key.webserverssh.public_key_openssh
-  }
 }
 
 resource "azurerm_public_ip" "bastionpip" {
@@ -129,7 +60,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "webserverss" {
     ip_configuration {
       name      = "nicconfiguration"
       primary   = true
-      subnet_id = azurerm_subnet.frontendsubnet.id
+      subnet_id = azurerm_subnet.backendsubnet.id
+      application_gateway_backend_address_pool_ids = azurerm_application_gateway.webserverag.backend_address_pool[*].id
     }
   }
 }
